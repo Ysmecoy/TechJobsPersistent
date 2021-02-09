af08393bdf96c52713be827300f8e1bdc9005448
Ysmecoy <72466944+Ysmecoy@users.noreply.github.com>
2021-02-09 16:06:25 -0600

    Update queries.sql

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,14 @@
 --Part 1
 
+/* Id integer, Name Varchar, EmployerId integer*/
+
 --Part 2
+
+/*select * from employers where Location = 'STL'*/
 
 --Part 3
 
+/*select s.Name,s.Description from skills as s
+inner join jobskills as js
+on s.Id = js.Skillid
+order by s.Name*/
